f13769498448075f666e09e31680fe31a91084f1
Rick <rbciti@msn.com>
2016-09-02 15:14:00 -0700

    Added delete functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
 
-  before_action :set_post, only:[:show, :edit, :update]
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
   
   def index
     @posts = Post.all.order("created_at ASC")   
   end
   
   def new 
-  @post = Post.new   
+  @post = Post.new 
+  end
+
+  def show    
   end
 
   def create
@@ -32,8 +35,10 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-  end
+ def destroy
+   @post.delete
+   redirect_to posts_path, notice: 'Your post was deleted successfully'
+ end
 
   private
 
