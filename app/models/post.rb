8ef59c5ec155eee7e4f5158f7fbcedd6d7d78c7c
Rick <rbciti@msn.com>
2016-09-06 12:13:23 -0700

    Added enum for status within posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
 
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
+  
   belongs_to :user
   
   validates_presence_of :date, :rationale
 end
+
+
+# statuses:  submitted, approved, rejected
