ebfe3c824ac70685cb33c561b38e15b5f68dfd5d
Rick <rbciti@msn.com>
2016-09-07 15:12:40 -0700

    Implemeted rspec for permissions on edits

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,9 +7,9 @@ describe 'navigate' do
   end
 
   describe 'index' do
-   before do
-     visit posts_path
-   end
+    before do
+      visit posts_path
+    end
 
     it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
@@ -25,10 +25,22 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+
+    it 'has a scope so that only post creators can see their posts' do
+      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+
+      visit posts_path
+
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
   end
 
-  describe "new" do
-    it "has a new link from the homepage" do
+  describe 'new' do
+    it 'has a link from the homepage' do
       visit root_path
 
       click_link("new_post_from_nav")
@@ -36,9 +48,11 @@ describe 'navigate' do
     end
   end
 
-  describe "delete" do
-    it "can be deleted" do
+  describe 'delete' do
+    it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      # TODO refactor
+      @post.update(user_id: @user.id)
       visit posts_path
 
       click_link("delete_post_#{@post.id}_from_index")
@@ -47,50 +61,56 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    before do      
+    before do
       visit new_post_path
-    end 
+    end
 
-    it 'has a new form that can be reached' do 
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'can be created from new form page' do      
+    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
+
       expect(page).to have_content("Some rationale")
     end
 
-    it 'will have a user associated with it' do
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "User_Association"
+      fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
 
-      expect(User.last.posts.last.rationale).to eq("User_Association")
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 
-  describe "edit" do
+  describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
-    it "can be reached by clicking edit on index page" do
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
-    end
-
-    it "can be edited" do
-      visit edit_post_path(@post)
+    it 'can be edited' do
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
-      expect(User.last.posts.last.rationale).to eq("Edited content")
+      expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
