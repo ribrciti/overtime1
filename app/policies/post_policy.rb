ab468203d71e20ac9d1106af8fe9269717bb44ba
Rick <rbciti@msn.com>
2016-09-08 12:03:52 -0700

    Block users after post marked complete

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,11 +1,21 @@
 class PostPolicy < ApplicationPolicy
 
   def update?
-    # Case 1 = User of record might need to edit a request for overtime
-    # Case 2 = Admin User, needs to edit submitted to approve or reject
-    record.user_id == user.id || admin_types.include?(user.type)
-
-    
-
+    return true if post_approved? && admin?   
+    return true if user_or_admin  && !post_approved?
   end
+
+  private
+
+    def user_or_admin
+      record.user_id == user.id || admin?
+    end
+
+    def admin?
+      admin_types.include?(user.type)    
+    end
+
+    def post_approved? 
+      record.approved?  
+    end
 end
