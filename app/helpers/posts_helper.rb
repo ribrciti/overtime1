67856ab1659243f6e229a8b59b08671efe0a03e1
Rick <rbciti@msn.com>
2016-09-08 19:12:10 -0700

    Upddated views to hide edit button once a post is approved

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,12 @@
 module PostsHelper
 
   def status_label status
-    status_span_generator
+    status_span_generator status
   end
 
   private
 
-    def status_span_generator
+    def status_span_generator status
       case status
       when 'submitted'
         content_tag(:span, status.titleize, class: 'label label-primary')
