743019abd7791b9cab8c9d6d33bf2212d1917d2c
Rick <rbciti@msn.com>
2016-09-21 10:00:59 -0700

    Integrated basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,8 +19,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   config.active_support.deprecation = :log
@@ -31,9 +29,11 @@ Rails.application.configure do
 
   config.assets.quiet = true
 
-
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
